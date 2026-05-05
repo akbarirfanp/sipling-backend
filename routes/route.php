0a92d5904ad5352463cc67ac9dbdd144d0d1263a
dugong bekasi <irfan.putra@solutif.co.id>
2026-05-05 16:55:00 +0700

    feat: crud fee management

--- routes/route.php
+++ routes/route.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\FeeController;
 
 Route::group(['prefix' => '/users'], function() {
     Route::post('/', [UserController::class, 'createUser']);
@@ -9,4 +10,12 @@ Route::group(['prefix' => '/users'], function() {
     Route::get('/{id?}', [UserController::class, 'getUserDetail']);
     Route::put('/{id}', [UserController::class, 'updateUser']);
     Route::delete('/{id}', [UserController::class, 'deleteUser']);
+});
+
+Route::group(['prefix' => '/fees'], function() {
+    Route::post('/', [FeeController::class, 'createFee']);
+    Route::get('/', [FeeController::class, 'getAllFee']);
+    Route::get('/{id?}', [FeeController::class, 'getFeeDetail']);
+    Route::put('/{id}', [FeeController::class, 'updateFee']);
+    Route::delete('/{id}', [FeeController::class, 'deleteFee']);
 });
